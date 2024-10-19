56704afdae5ead8f9eb0f49d672d86d91fde1441
Sumit <tonystark83033@gmail.com>
2024-10-19 15:56:24 +0530

    feat:[Add about section in landing  page]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Hero from "@/components/customComponents/landingPage/Hero";
+import About from "@/components/customComponents/landingPage/About";
 export default function Home() {
   return (
 <>
 <Hero></Hero>
+<About></About>
 </>
   );
 }
