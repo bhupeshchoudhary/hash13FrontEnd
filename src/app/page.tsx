a2d88fc3f6e7c85ebafeb2c4ed2da42c8bcab728
Sumit <tonystark83033@gmail.com>
2024-10-21 11:32:03 +0530

    core:[Add all the component of landing page]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
 import Hero from "@/components/customComponents/landingPage/Hero";
 import About from "@/components/customComponents/landingPage/About";
+import CoreFeature from "@/components/customComponents/landingPage/CoreFeature";
+import PopularTopic from "@/components/customComponents/landingPage/PopularTopic";
+import BlogSection from "@/components/customComponents/landingPage/Blog";
 export default function Home() {
   return (
 <>
 <Hero></Hero>
 <About></About>
+<CoreFeature></CoreFeature>
+<PopularTopic></PopularTopic>
+<BlogSection></BlogSection>
 </>
   );
 }
