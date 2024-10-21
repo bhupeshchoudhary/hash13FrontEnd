1486af564e409de536d22f982b55ecc5bde5d1a2
Sumit <tonystark83033@gmail.com>
2024-10-21 12:18:15 +0530

    feat:[Add Testimonials and SubscribeNewsLatter in main page.tsx]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,17 @@ import About from "@/components/customComponents/landingPage/About";
 import CoreFeature from "@/components/customComponents/landingPage/CoreFeature";
 import PopularTopic from "@/components/customComponents/landingPage/PopularTopic";
 import BlogSection from "@/components/customComponents/landingPage/Blog";
+import Testimonials from "@/components/customComponents/landingPage/Testimonials";
+import Subscribe from "@/components/customComponents/landingPage/SubscribeNewsLatter";
 export default function Home() {
   return (
 <>
 <Hero></Hero>
 <About></About>
 <CoreFeature></CoreFeature>
+<Subscribe></Subscribe>
 <PopularTopic></PopularTopic>
+<Testimonials></Testimonials>
 <BlogSection></BlogSection>
 </>
   );
