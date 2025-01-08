1e485f35e0bcb98e7466db43aace6afe5086ab90
Bhupesh Choudhary <bhaibhupesh10@gmail.com>
2025-01-08 05:49:36 +0530

    updated homepage testimonials avatar

--- data/testimonials/posts.tsx
+++ data/testimonials/posts.tsx
@@ -9,7 +9,7 @@ export const landingPageTestimonialPosts: Post[] = [
       author: {
         name: "Akshata Tiwari",
         title: "MERN Enthusiast Seeking Opportunities to Develop and Enhance my Skills",
-        avatar: "https://example.com/avatar1.jpg"
+        avatar: "https://media.licdn.com/dms/image/v2/D4D03AQHXkuvnF5Zm7g/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1724997745281?e=1741824000&v=beta&t=4L3p08LUosAQ3DGvDfuJ3VVdjuPLoY3hgzwy7A7rTQQ"
       },
       content: `Hey Folks...`,
       timestamp: "8 Mar 2023",
@@ -20,7 +20,7 @@ export const landingPageTestimonialPosts: Post[] = [
       author: {
         name: "Ayushi Jain",
         title: "Branding | Marketing and Strategy",
-        avatar: "https://example.com/avatar2.jpg"
+        avatar: "https://media.licdn.com/dms/image/v2/D4D03AQHXkuvnF5Zm7g/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1724997745281?e=1741824000&v=beta&t=4L3p08LUosAQ3DGvDfuJ3VVdjuPLoY3hgzwy7A7rTQQ"
       },
       content: `From Google searches...`,
       timestamp: "10 Mar 2023"
@@ -30,7 +30,7 @@ export const landingPageTestimonialPosts: Post[] = [
       author: {
         name: "Prashant Hada",
         title: "Student at Ronald University of Applied Sciences",
-        avatar: "https://example.com/avatar3.jpg"
+        avatar: "https://media.licdn.com/dms/image/v2/D4D03AQHXkuvnF5Zm7g/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1724997745281?e=1741824000&v=beta&t=4L3p08LUosAQ3DGvDfuJ3VVdjuPLoY3hgzwy7A7rTQQ"
       },
       content: `Happy to announce the completion of the Power BI workshop. Thanks to @LinuxWorld and @DeekshaAnand for the guidance and knowledge transfer. #PowerBI #DataAnalytics`,
       timestamp: "15 Mar 2023"
@@ -40,7 +40,7 @@ export const landingPageTestimonialPosts: Post[] = [
       author: {
         name: "Nikita Chawre",
         title: "Data Analysis Enthusiast",
-        avatar: "https://example.com/avatar4.jpg"
+        avatar: "https://media.licdn.com/dms/image/v2/D4D03AQHXkuvnF5Zm7g/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1724997745281?e=1741824000&v=beta&t=4L3p08LUosAQ3DGvDfuJ3VVdjuPLoY3hgzwy7A7rTQQ"
       },
       content: `Updated my dashboard with a new color scheme for better UX! Appreciate the feedback from the community. #DataVisualization #UserExperience`,
       timestamp: "18 Mar 2023"
@@ -51,7 +51,7 @@ export const landingPageTestimonialPosts: Post[] = [
       author: {
         name: "Nikita Chawre",
         title: "Data Analysis Enthusiast",
-        avatar: "https://example.com/avatar4.jpg"
+        avatar: "https://media.licdn.com/dms/image/v2/D4D03AQHXkuvnF5Zm7g/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1724997745281?e=1741824000&v=beta&t=4L3p08LUosAQ3DGvDfuJ3VVdjuPLoY3hgzwy7A7rTQQ"
       },
       content: `Updated my dashboard with a new color scheme for better UX! Appreciate the feedback from the community. #DataVisualization #UserExperience`,
       timestamp: "18 Mar 2023"
@@ -61,7 +61,7 @@ export const landingPageTestimonialPosts: Post[] = [
       author: {
         name: "Akshata Tiwari",
         title: "MERN Enthusiast Seeking Opportunities to Develop and Enhance my Skills",
-        avatar: "https://example.com/avatar1.jpg"
+        avatar: "https://media.licdn.com/dms/image/v2/D4D03AQHXkuvnF5Zm7g/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1724997745281?e=1741824000&v=beta&t=4L3p08LUosAQ3DGvDfuJ3VVdjuPLoY3hgzwy7A7rTQQ"
       },
       content: `Hey Folks...`,
       timestamp: "8 Mar 2023",
@@ -72,7 +72,7 @@ export const landingPageTestimonialPosts: Post[] = [
       author: {
         name: "Nikita Chawre",
         title: "Data Analysis Enthusiast",
-        avatar: "https://example.com/avatar4.jpg"
+        avatar: "https://media.licdn.com/dms/image/v2/D4D03AQHXkuvnF5Zm7g/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1724997745281?e=1741824000&v=beta&t=4L3p08LUosAQ3DGvDfuJ3VVdjuPLoY3hgzwy7A7rTQQ"
       },
       content: `Updated my dashboard with a new color scheme for better UX! Appreciate the feedback from the community. #DataVisualization #UserExperience`,
       timestamp: "18 Mar 2023"
@@ -82,7 +82,7 @@ export const landingPageTestimonialPosts: Post[] = [
       author: {
         name: "Nikita Chawre",
         title: "Data Analysis Enthusiast",
-        avatar: "https://example.com/avatar4.jpg"
+        avatar: "https://media.licdn.com/dms/image/v2/D4D03AQHXkuvnF5Zm7g/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1724997745281?e=1741824000&v=beta&t=4L3p08LUosAQ3DGvDfuJ3VVdjuPLoY3hgzwy7A7rTQQ"
       },
       content: `Updated my dashboard with a new color scheme for better UX! Appreciate the feedback from the community. #DataVisualization #UserExperience`,
       timestamp: "18 Mar 2023"
@@ -92,7 +92,7 @@ export const landingPageTestimonialPosts: Post[] = [
       author: {
         name: "Akshata Tiwari",
         title: "MERN Enthusiast Seeking Opportunities to Develop and Enhance my Skills",
-        avatar: "https://example.com/avatar1.jpg"
+        avatar: "https://media.licdn.com/dms/image/v2/D4D03AQHXkuvnF5Zm7g/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1724997745281?e=1741824000&v=beta&t=4L3p08LUosAQ3DGvDfuJ3VVdjuPLoY3hgzwy7A7rTQQ"
       },
       content: `Hey Folks...`,
       timestamp: "8 Mar 2023",
