851ffc103853859507ee3b677fe12ca88d117bcc
Sumit <tonystark83033@gmail.com>
2025-01-21 23:20:01 +0530

    fix:[type error]

--- src/app/courses/[courseId]/page.tsx
+++ src/app/courses/[courseId]/page.tsx
@@ -120,7 +120,7 @@ export default function CourseDetailPage() {
         </section>
 
         <section className=" ">
-          <Tools course={course} />
+          <Tools />
         </section>
 
         <section className="">
