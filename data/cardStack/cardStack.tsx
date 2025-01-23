e02d5ae7c3a42b6ce11a2f4ee4382f6e96e1287c
Bhupesh Choudhary <bhaibhupesh10@gmail.com>
2025-01-23 17:04:29 +0530

    updated code

--- data/cardStack/cardStack.tsx
+++ data/cardStack/cardStack.tsx
@@ -1,71 +1,71 @@
-import { Module, Learner, } from "../../types/cardStack";
+// import { Module, Learner, } from "../../types/cardStack";
 
-export const modules: Module[] = [
-    {
-      number: "Module 1",
-      title: "Build Your Foundation",
-      videos: 7,
-      duration: "32 mins",
-      features: [
-        "Clearing your fundamentals and unlearning BS",
-        "Overcome communication challenges and speak fluently",
-        "Common mistakes & roadblocks and how to avoid them",
-      ],
-      colors: {
-        background: "bg-gradient-to-br from-blue-50 to-blue-100",
-        text: "text-blue-900",
-        badge: "bg-blue-100",
-        badgeText: "text-blue-900",
-      }
-    },
-    {
-      number: "Module 2",
-      title: "Learn the science behind communication",
-      videos: 8,
-      duration: "1.5 hour",
-      features: [
-        "Discover the science of storytelling to make your message memorable",
-        "Breakdown Hitler's powerful speech tactics of that time using science of Story Part 1",
-        "Learn from case studies of big brands like Coca-Cola and Cadbury",
-      ],
-      colors: {
-        background: "bg-gradient-to-br from-purple-50 to-purple-100",
-        text: "text-purple-900",
-        badge: "bg-purple-100",
-        badgeText: "text-purple-900",
-      }
-    },
-    {
-      number: "Module 3",
-      title: "Craft and Structure Your Message",
-      videos: 11,
-      duration: "2 hour",
-      features: [
-        "Learn to structure your message and keep your audience engaged throughout",
-        "The Spider Blue and Clear 3 keys structure that Sandeep uses to write his speeches",
-        "Discover tips on how to craft your talk from great speakers like Simon Sinek",
-      ],
-      colors: {
-        background: "bg-gradient-to-br from-emerald-50 to-emerald-100",
-        text: "text-emerald-900",
-        badge: "bg-emerald-100",
-        badgeText: "text-emerald-900",
-      }
-    },
+// export const modules: Module[] = [
+//     {
+//       number: "Module 1",
+//       title: "Build Your Foundation",
+//       videos: 7,
+//       duration: "32 mins",
+//       features: [
+//         "Clearing your fundamentals and unlearning BS",
+//         "Overcome communication challenges and speak fluently",
+//         "Common mistakes & roadblocks and how to avoid them",
+//       ],
+//       colors: {
+//         background: "bg-gradient-to-br from-blue-50 to-blue-100",
+//         text: "text-blue-900",
+//         badge: "bg-blue-100",
+//         badgeText: "text-blue-900",
+//       }
+//     },
+//     {
+//       number: "Module 2",
+//       title: "Learn the science behind communication",
+//       videos: 8,
+//       duration: "1.5 hour",
+//       features: [
+//         "Discover the science of storytelling to make your message memorable",
+//         "Breakdown Hitler's powerful speech tactics of that time using science of Story Part 1",
+//         "Learn from case studies of big brands like Coca-Cola and Cadbury",
+//       ],
+//       colors: {
+//         background: "bg-gradient-to-br from-purple-50 to-purple-100",
+//         text: "text-purple-900",
+//         badge: "bg-purple-100",
+//         badgeText: "text-purple-900",
+//       }
+//     },
+//     {
+//       number: "Module 3",
+//       title: "Craft and Structure Your Message",
+//       videos: 11,
+//       duration: "2 hour",
+//       features: [
+//         "Learn to structure your message and keep your audience engaged throughout",
+//         "The Spider Blue and Clear 3 keys structure that Sandeep uses to write his speeches",
+//         "Discover tips on how to craft your talk from great speakers like Simon Sinek",
+//       ],
+//       colors: {
+//         background: "bg-gradient-to-br from-emerald-50 to-emerald-100",
+//         text: "text-emerald-900",
+//         badge: "bg-emerald-100",
+//         badgeText: "text-emerald-900",
+//       }
+//     },
         
-  ];
+//   ];
 
 
 
-  export const learners: Learner[] = [
-    { name: "ShareChat Learner", company: "ShareChat" },
-    { name: "Google Learner", company: "Google" },
-    { name: "Apna Learner", company: "Apna" },
-    { name: "Capgemini Learner", company: "Capgemini" },
-  ];
+//   export const learners: Learner[] = [
+//     { name: "ShareChat Learner", company: "ShareChat" },
+//     { name: "Google Learner", company: "Google" },
+//     { name: "Apna Learner", company: "Apna" },
+//     { name: "Capgemini Learner", company: "Capgemini" },
+//   ];
 
-  export const benefits: string[] = [
-    "Stay ahead in your field by mastering industry relevant skills through our online sessions",
-    "Dive into real challenges from today's businesses, gaining hands-on experience",
-    "Tap into a wealth of career opportunities through our established network",
-  ]
+//   export const benefits: string[] = [
+//     "Stay ahead in your field by mastering industry relevant skills through our online sessions",
+//     "Dive into real challenges from today's businesses, gaining hands-on experience",
+//     "Tap into a wealth of career opportunities through our established network",
+//   ]
