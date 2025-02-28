1e9f03e55eb65b1cad008f047c4c76dd15036868
Bhupesh Choudhary <bhaibhupesh10@gmail.com>
2025-02-28 19:45:38 +0530

    updated code

--- data/courses/courses.tsx
+++ data/courses/courses.tsx
@@ -1,12 +1,15 @@
 // data/courses/courses.ts
 import { Course } from "../../types/courses";
 
-import {specialisationInPrometheusGrafana} from  "./specialisation-in-prometheus-grafana"
-import {specialisationInDevops} from "./specialisation-in-devops"
+import {specialisationInPrometheusGrafana} from  "./specialisation-in-prometheus-grafana";
+import {specialisationInDevops} from "./specialisation-in-devops";
+import { specialisationInShellScripting } from "./specialisation-in-shell-scripting";
+
 export const courseInfo: Course[] = [
  
   specialisationInDevops,
   specialisationInPrometheusGrafana,
+  specialisationInShellScripting,
 
 
 
