be240291ee1d4e7020563229be9ea2e8759a33e2
Sumit <tonystark83033@gmail.com>
2025-02-28 19:46:03 +0530

    add splunk

--- data/courses/courses.tsx
+++ data/courses/courses.tsx
@@ -3,10 +3,12 @@ import { Course } from "../../types/courses";
 
 import {specialisationInPrometheusGrafana} from  "./specialisation-in-prometheus-grafana"
 import {specialisationInDevops} from "./specialisation-in-devops"
+import {SplunkAdmin} from "./SplunkAdministration"
 export const courseInfo: Course[] = [
  
   specialisationInDevops,
   specialisationInPrometheusGrafana,
+  SplunkAdmin,
 
 
 
