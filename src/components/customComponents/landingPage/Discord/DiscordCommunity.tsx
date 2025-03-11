3f804c6545a640675723aded6fb08cc1e59f652d
Sumit <tonystark83033@gmail.com>
2025-03-11 16:19:13 +0530

    fix discord

--- src/components/customComponents/landingPage/Discord/DiscordCommunity.tsx
+++ src/components/customComponents/landingPage/Discord/DiscordCommunity.tsx
@@ -92,7 +92,7 @@ export default function DiscordCommunity() {
       alt="Code editor screenshot"
       width={300}
       height={220}
-      className={`border-4 rounded-lg shadow-lg ml-[50%] transform transition-all duration-1000 ease-out ${
+      className={`border-4 rounded-lg shadow-lg ml-[40%] transform transition-all duration-1000 ease-out ${
       isIntersecting 
       ? "translate-y-[-20px] translate-x-[-30px]"
       : "translate-y-[30px] translate-x-0"
@@ -106,7 +106,7 @@ export default function DiscordCommunity() {
         alt="Code editor screenshot"
         width={300}
         height={220}
-        className={`border-4 absolute bottom-[60%] left-[10%] rounded-lg shadow-lg transform transition-all duration-1000 ease-out ${
+        className={`border-4 absolute bottom-[50%] left-[-10%] rounded-lg shadow-lg transform transition-all duration-1000 ease-out ${
       isIntersecting
         ? "translate-y-[-0px] translate-x-[10px]"
         : "translate-y-[80px] translate-x-10"
